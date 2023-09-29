c8ed909283354f0b95c61ba6fb35bcf14338be1b
DESKTOP-QOEPV8D\Ethan Cornish <ethan.cornish@gmail.com>
2023-09-29 22:50:38 +1000

    Completed basic graphs for fig 3+4. Started fig 5

---
+++ vega_lite_vis.js
@@ -0,0 +1,25 @@
+// Each Chart is a variable set to the JSON file and relative path
+var vg_x = "Charts/daily_cost_bar_chart.vg.json";
+
+// Puts the variable (JSON graph obj) in the HTML tag with it bar_chart
+// Each chart is then attached to a HTML element via the vegaEmbed function
+//  vegaEmbed("#html_tag_id", chart_json_path_variable)...
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+}).catch(console.error);
+
+var spec2 = "js/daily_cost_bar_chart.json";
+vegaEmbed('#vega_bar_chart', spec2).then(function(result) {
+}).catch(console.error);
+
+
+var vg_1 = "Charts/fig1_routes_map.vg.json";
+var vg_2 = "Charts/fig2_main_connections_sankey.vg.json";
+var vg_3 = "Charts/fig3_biggest_hubs_bar.vg.json";
+var vg_4 = "Charts/fig4_plane_sizes_scatter.vg.json";
+var vg_5 = "Charts/fig5_each_city_bar.vg.json";
+
+vegaEmbed("#figure_1", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#figure_2", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#figure_3", vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#figure_4", vg_4).then(function(result) {}).catch(console.error);
+vegaEmbed("#figure_5", vg_5).then(function(result) {}).catch(console.error);
